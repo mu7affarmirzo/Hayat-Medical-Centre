8db5272937fd6002aa727b8e6ed523ef080eb1c6
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2022-09-21 03:24:51 +0500

    .gitignore is now working 2

--- backend/api/v1/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-app_name = 'account'
-
-urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-]
