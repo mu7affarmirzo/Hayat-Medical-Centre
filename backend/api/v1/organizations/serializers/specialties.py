6dc41e9d569b08afe43391f0fb1831122698beb1
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2022-11-05 15:13:18 +0500

    crud doc specialties.py

--- backend/api/v1/organizations/serializers/specialties.py
+++ backend/api/v1/organizations/serializers/specialties.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 from account.models import SpecialityModel, DoctorSpecialityModel
 
 
+# class CreateSerializer(serializers.ModelSerializer):
+#
+#     class
+
 class SpecialtiesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpecialityModel
@@ -18,3 +22,20 @@ class SpecialtiesCreateSerializer(serializers.ModelSerializer):
         model = SpecialityModel
         fields = "__all__"
 
+
+class DoctorSpecialitiesListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DoctorSpecialityModel
+        fields = [
+            "id",
+            "name",
+            "color"
+        ]
+
+
+class DoctorSpecialitiesCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DoctorSpecialityModel
+        fields = "__all__"
--- backend/api/v1/organizations/views/specialties.py
+++ backend/api/v1/organizations/views/specialties.py
@@ -5,8 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
-from account.models import SpecialityModel
-from api.v1.organizations.serializers.specialties import SpecialtiesListSerializer
+from account.models import SpecialityModel, DoctorSpecialityModel
+from api.v1.organizations.serializers.specialties import SpecialtiesListSerializer, SpecialtiesCreateSerializer, \
+    DoctorSpecialitiesListSerializer, DoctorSpecialitiesCreateSerializer
 from rest_framework.generics import ListAPIView
 
 
@@ -18,12 +19,32 @@ class SpecialtiesView(APIView):
         serializer = SpecialtiesListSerializer(specialities, many=True)
         return Response(serializer.data)
 
-    @swagger_auto_schema(tags=['organizations'])
+    @swagger_auto_schema(tags=['organizations'], request_body=SpecialtiesCreateSerializer)
     @permission_classes((IsAuthenticated, ))
     def post(self, request, format=None):
         creator = request.user
         specialty = SpecialityModel(created_by=creator)
         serializer = SpecialtiesListSerializer(specialty, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DoctorSpecialitiesView(APIView):
+
+    @swagger_auto_schema(tags=['organizations'])
+    def get(self, request, format=None):
+        specialities = DoctorSpecialityModel.objects.all()
+        serializer = DoctorSpecialitiesListSerializer(specialities, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(tags=['organizations'], request_body=DoctorSpecialitiesCreateSerializer)
+    @permission_classes((IsAuthenticated, ))
+    def post(self, request, format=None):
+        creator = request.user
+        specialty = DoctorSpecialityModel(created_by=creator)
+        serializer = DoctorSpecialitiesCreateSerializer(specialty, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
