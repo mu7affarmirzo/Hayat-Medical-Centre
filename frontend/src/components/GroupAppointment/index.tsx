57bc3eaa7d39c5c2591c7ff0183182af7944e7b9
theAkbarov <asrorjon5538@gmail.com>
2022-11-07 11:46:19 +0500

    Registration Pop-up menus initialized

--- frontend/src/components/GroupAppointment/index.tsx
+++ frontend/src/components/GroupAppointment/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Grid from "@mui/material/Unstable_Grid2";
+import { ReactComponent as CalendarEdit } from "../../assets/img/note-add.svg";
+import { ReactComponent as SearchIcon } from '../../assets/img/header_icons/search-normal.svg';
+import { ReactComponent as EditIcon } from '../../assets/img/edit.svg';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 import classes from './GroupAppointment.module.scss';
-import { Button, FormControl, MenuItem, Paper, Select, TextField } from '@mui/material';
-import { ReactComponent as SearchIcon } from '../../assets/img/header_icons/search-normal.svg';
 
 const GroupAppointment = () => {
     return (
@@ -11,15 +13,28 @@ const GroupAppointment = () => {
             <h2 className={classes.title}>Добавить новой записи</h2>
             <Grid container spacing={10} >
                 <Grid md={8}>
-                    <FormControl fullWidth className={classes.formControl}>
-                        <TextField
-                            name='id'
-                            // onChange={handleTextFieldChange}
-                            label="Выберите клиента "
-                            type="text"
-                        />
-                    </FormControl>
+                    <div className={classes.row}>
+                        <FormControl fullWidth className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Выберите клиента</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Выберите клиента"
+                            >
+                                <MenuItem value={'Лебедев Сергей Васильевич'}>Лебедев Сергей Васильевич</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button variant='outlined'>
+                            <CalendarEdit />
+                        </Button>
+                        <Button variant='outlined'>
+                            <EditIcon />
+                        </Button>
+                    </div>
                     <FormControl style={{ marginBottom: 10 }} fullWidth className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Выберите пакет</InputLabel>
                         <Select label='Выберите пакет' labelId="demo-simple-select-label" id="demo-simple-select">
                             <MenuItem value={'Не выбрана'}>Не выбрана</MenuItem>
                             <MenuItem value={10}>Ten</MenuItem>
@@ -28,12 +43,16 @@ const GroupAppointment = () => {
                 </Grid>
                 <Grid md={4}>
                     <FormControl fullWidth className={classes.formControl}>
-                        <TextField
-                            name='id'
-                            // onChange={handleTextFieldChange}
-                            label="Стоимость пакета "
-                            type="text"
-                        />
+                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
                     </FormControl>
                     <Button size='large' color='primary' variant='contained' fullWidth>
                         Авто расстановка
@@ -47,6 +66,26 @@ const GroupAppointment = () => {
                             <SearchIcon />
                             <TextField style={{ all: 'unset', width: '100%' }} type={'search'} placeholder='Поиск по таблице' fullWidth />
                         </div>
+                        <table className={classes.tableBody}>
+                            <thead>
+                                <th></th>
+                                <th>Услуга</th>
+                                <th>Специалность</th>
+                                <th>Выберите врача</th>
+                                <th>Дата приёма</th>
+                                <th>Время начала приёма</th>
+                                <th>Время окончания приёма</th>
+                            </thead>
+                            <tr>
+                                <td></td>
+                                <td >{'Однозначно, стремящиеся вытеснить традиционное производство, нанотехнологии представляют собой не что иное, как квинтэссенцию победы маркетинга над разумом и должны быть описаны максимально подробно.'}</td>
+                                <td>{'Невролог '}</td>
+                                <td>{'нет данных'}</td>
+                                <td>{'09.08.2022'}</td>
+                                <td>{'15:23'}</td>
+                                <td>{'15:33'}</td>
+                            </tr>
+                        </table>
                     </div>
                 </Grid>
             </Grid>
--- frontend/src/components/sideBar.tsx
+++ frontend/src/components/sideBar.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React from "react";
 import styles from "../views/main/index.module.scss";
-import {NavBarDropdowns} from "../consts/main";
-import parse from 'html-react-parser';
-import { useLocation } from 'react-router';
+import { NavBarDropdowns } from "../consts/main";
+import parse from "html-react-parser";
+import { useLocation, useNavigate } from "react-router";
+import Modal from "./Modal";
+import GroupAppointment from "./GroupAppointment";
 
 const SideBar = () => {
-    const location = useLocation()
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [groupAppointment, setGroupAppointment] = React.useState(false);
+
+    const clickHandler = (path: string) => {
+        if (path === "/groupAppointment") {
+            setGroupAppointment(true);
+        } else {
+            navigate(path);
+        }
+    };
+
     return (
         <div className={styles.sideBar}>
-            {NavBarDropdowns[0]?.dropdown?.map(item => (
-                <div  className={`${styles.sideBar_item} ${item.path === location.pathname ? styles.active : ""}`}>
-                    {item.img}
-                    <span>{parse(item.sideBarText || item.text)}</span>
-                </div>
-            ))}
-        </div>
-    );
+          {groupAppointment ? (
+              <Modal
+                  show={groupAppointment}
+                  onClose={() => setGroupAppointment(false)}
+              >
+                  <GroupAppointment />
+              </Modal>
+          ) : null}
+          {NavBarDropdowns[0]?.dropdown?.map((item) => (
+              <div
+                  onClick={() => clickHandler(item.path)}
+                  className={`${styles.sideBar_item} ${item.path === location.pathname ? styles.active : ""
+                      }`}
+              >
+                  {item.img}
+                  <span>{parse(item.sideBarText || item.text)}</span>
+              </div>
+          ))}
+      </div>
+  );
 };
 
 export default SideBar;
