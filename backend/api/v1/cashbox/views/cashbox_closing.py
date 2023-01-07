ccb7545dc3a46225308de119b79c6ed132700c27
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2023-01-08 03:46:28 +0500

    Implement start & end date filtering to closing histories

--- backend/api/v1/cashbox/views/cashbox_closing.py
+++ backend/api/v1/cashbox/views/cashbox_closing.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,7 +18,9 @@ class CashBoxView(APIView):
 
     @swagger_auto_schema(tags=['cashbox'])
     def get(self, request, format=None):
-        cashbox = CashBoxClosingHistoryRecordsModel.objects.all()
+        start_date = self.request.query_params.get("start_date", datetime.today().date())
+        end_date = self.request.query_params.get("end_date", datetime.today().date())
+        cashbox = CashBoxClosingHistoryRecordsModel.objects.filter(created_at__range=(start_date, end_date))
         serializer = CashBoxSerializer(cashbox, many=True)
         return Response(serializer.data)
 
