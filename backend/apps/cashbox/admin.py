de39ca43c9bf8df08e8dd468064c00d4d66e65af
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2023-01-08 17:30:41 +0500

    Updated: receipt admin

--- backend/apps/cashbox/admin.py
+++ backend/apps/cashbox/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import receipt, cashbox, transactions
 
 
-admin.site.register(receipt.ReceiptModel)
+# admin.site.register(receipt.ReceiptModel)
+
+@admin.register(receipt.ReceiptModel)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in receipt.ReceiptModel._meta.fields]
+
+
 admin.site.register(cashbox.CashBoxClosingHistoryRecordsModel)
 admin.site.register(transactions.TransactionsModel)
 admin.site.register(transactions.AppointmentServiceTransactionsModel)
