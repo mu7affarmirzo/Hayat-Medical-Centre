501b89ffd9425ec38e454349dd1285ef02a07c22
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2023-01-16 16:38:45 +0500

    Updated: service setup

--- backend/api/v1/appointment/urls.py
+++ backend/api/v1/appointment/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from api.v1.appointment.views.appointment import AppointmentsModelView, AppointmentsRetrieveView
-from api.v1.appointment.views.appointment_service import AppointmentServiceRetrieveView, AppointmentServiceView, \
-    setup_service
+from api.v1.appointment.views.appointment_service import AppointmentServiceRetrieveView, AppointmentServiceView
 
 from api.v1.appointment.views.appointment import appointment_time_view
 
@@ -11,7 +10,7 @@ urlpatterns = [
     path('', AppointmentsModelView.as_view(), name='appointment'),
     path('<int:pk>', AppointmentsRetrieveView.as_view(), name='appointment'),
     path('service/', AppointmentServiceView.as_view(), name='appointment-service'),
-    path('service-setup/', setup_service, name='appointment-service'),
+    # path('service-setup/', setup_service, name='appointment-service'),
     path('service/<int:pk>', AppointmentServiceRetrieveView.as_view(), name='appointment-service'),
     path('appointments-time-view/', appointment_time_view, name='appointments-time-view'),
 
