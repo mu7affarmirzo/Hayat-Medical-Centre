b3ba00ff456c826f659e267b14f898eeb0d55905
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-05-15 10:14:00 +0500

    logus models

--- backend/apps/logus/admin.py
+++ backend/apps/logus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.logus.models import *
 
 # Register your models here.
+admin.site.register(RoomModel)
+admin.site.register(BookedRoomModel)
+admin.site.register(TariffModel)
+admin.site.register(RoomTypeModel)
+admin.site.register(BookingModel)
+admin.site.register(RoomTariffModel)
+admin.site.register(ServiceModel)
+admin.site.register(TariffServiceModel)
