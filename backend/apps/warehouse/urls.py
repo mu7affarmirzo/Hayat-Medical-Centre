d7bb42c87f2e224331839dce618775ff462988e9
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-06-20 20:20:41 +0500

    minor changes

--- backend/apps/warehouse/urls.py
+++ backend/apps/warehouse/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-from apps.warehouse.views import LoginViewStatic, IndexView, IncomeView, MedicinesView
+from apps.warehouse.views import login_view, index_view, IncomeView, MedicinesView, logout_view
 
 app_name = 'warehouse'
 
 urlpatterns = [
-    path('login/', LoginViewStatic.as_view(), name='warehouse-login'),
-    path('index/', IndexView.as_view(), name='warehouse-index'),
+    path('login/', login_view, name='warehouse-login'),
+    path('logout/', logout_view, name='warehouse-logout'),
+    path('index/', index_view, name='warehouse-index'),
     path('incomes/', IncomeView.as_view(), name='warehouse-incomes'),
     path('medicines/', MedicinesView.as_view(), name='warehouse-medicines'),
 ]
--- backend/apps/warehouse/views.py
+++ backend/apps/warehouse/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-
+from apps.warehouse.forms import AccountAuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from apps.account.models import OrganizationModel
 
 # ------------------------------
 #           TASKS
@@ -10,13 +13,41 @@ from django.views.generic import TemplateView
 
 """
 
+
 # Create your views here.
-class LoginViewStatic(TemplateView):
-    template_name = 'warehouse/login.html'
+def login_view(request):
+    context = {}
+    user = request.user
+    print("started")
+    if user.is_authenticated:
+        return redirect("warehouse:warehouse-index")
+    print("asdf")
+    print(request.POST)
+    if request.POST:
+        print("request.POST")
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            print(email, password)
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("warehouse:warehouse-index")
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return render(request, 'warehouse/login.html', context)
 
 
-class IndexView(TemplateView):
-    template_name = 'warehouse/index.html'
+@login_required
+def index_view(request):
+    context = {}
+    orgs = OrganizationModel.objects.all()
+    context["orgs"] = orgs
+    return render(request, 'warehouse/index.html', context)
 
 
 class IncomeView(TemplateView):
@@ -25,3 +56,9 @@ class IncomeView(TemplateView):
 
 class MedicinesView(TemplateView):
     template_name = 'warehouse/medicines.html'
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('warehouse:warehouse-login')
