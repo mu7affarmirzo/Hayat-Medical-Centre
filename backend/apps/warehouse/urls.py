61598bd098480f3efb0afb0df66ac5d2455458dc
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2023-06-29 17:49:28 +0500

    🔄Updated: view structure🧮

--- backend/apps/warehouse/urls.py
+++ backend/apps/warehouse/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 
-from apps.warehouse.views import login_view, index_view, medicines_view, logout_view, incomes_view
+from apps.warehouse.views import main
 
 app_name = 'warehouse'
 
 urlpatterns = [
-    path('login/', login_view, name='warehouse-login'),
-    path('logout/', logout_view, name='warehouse-logout'),
-    path('index/', index_view, name='warehouse-index'),
-    path('incomes/', incomes_view, name='warehouse-incomes'),
-    path('incomes/<int:pk>', incomes_view, name='warehouse-incomes'),
-    path('medicines/', medicines_view, name='warehouse-medicines'),
-    path('medicines/<int:pk>', medicines_view, name='warehouse-medicines'),
+    path('login/', main.login_view, name='warehouse-login'),
+    path('logout/', main.logout_view, name='warehouse-logout'),
+    path('index/', main.index_view, name='warehouse-index'),
+    path('incomes/', main.incomes_view, name='warehouse-incomes'),
+    path('incomes/<int:pk>', main.incomes_view, name='warehouse-incomes'),
+    path('medicines/', main.medicines_view, name='warehouse-medicines'),
+    path('medicines/<int:pk>', main.medicines_view, name='warehouse-medicines'),
 ]
