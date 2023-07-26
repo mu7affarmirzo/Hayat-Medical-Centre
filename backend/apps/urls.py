f7dfab9de18b60158b0be6506fcb5295078c22df
Johadev05 <javohirsidikxonov@gmail.com>
2023-07-26 10:52:08 +0500

    Update urls.py

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('warehouse/', include('apps.warehouse.urls')),
-    # path('hospital/', include('apps.hospital.urls')),
+    path('hospital/', include('apps.hospital.urls')),
 
 ]
