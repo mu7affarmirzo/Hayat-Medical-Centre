a81e05701075df93a5d1320153d47ab03aca8d24
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2023-07-26 14:47:53 +0500

    admin

--- backend/apps/warehouse/admin.py
+++ backend/apps/warehouse/admin.py
@@ -1,17 +1,42 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from apps.warehouse.models import *
 
+def create_admin_class(model):
+    return type(
+        f"{model.__name__}Admin",
+        (ImportExportModelAdmin, admin.ModelAdmin),
+        {'list_display': [field.name for field in model._meta.fields]}
+    )
+for model in [
+    ItemsModel, ReceivedItemsModel, ReceiveRegistryModel, SendRegistryModel,
+    IncomeModel, IncomeItemsModel, ItemsInStockModel, SentItemsModel,
+    StorePointModel, WarehouseChequeModel, ChequeItemsModel,
+]:
+    admin.site.register(model, admin_class=type(
+        f"{model.__name__}Admin",
+        (ImportExportModelAdmin, admin.ModelAdmin),
+        {'list_display': [field.name for field in model._meta.fields]}
+    ))
 
-admin.site.register(ItemsModel)
-admin.site.register(ReceivedItemsModel)
-admin.site.register(ReceiveRegistryModel)
-admin.site.register(SendRegistryModel)
-admin.site.register(IncomeModel)
-admin.site.register(IncomeItemsModel)
-admin.site.register(ItemsInStockModel)
-admin.site.register(CompanyModel)
-admin.site.register(SentItemsModel)
-admin.site.register(StorePointModel)
-admin.site.register(WarehouseChequeModel)
-admin.site.register(ChequeItemsModel)
+# admin.site.register(ItemsModel, create_admin_class(ItemsModel))
+# admin.site.register(ReceivedItemsModel, create_admin_class(ReceivedItemsModel))
+# admin.site.register(ReceiveRegistryModel, create_admin_class(ReceiveRegistryModel))
+# admin.site.register(SendRegistryModel, create_admin_class(SendRegistryModel))
+# admin.site.register(IncomeModel, create_admin_class(IncomeModel))
+# admin.site.register(IncomeItemsModel, create_admin_class(IncomeItemsModel))
+# admin.site.register(ItemsInStockModel, create_admin_class(ItemsInStockModel))
+# admin.site.register(SentItemsModel, create_admin_class(SentItemsModel))
+# admin.site.register(StorePointModel, create_admin_class(StorePointModel))
+# admin.site.register(WarehouseChequeModel, create_admin_class(WarehouseChequeModel))
+# admin.site.register(ChequeItemsModel, create_admin_class(ChequeItemsModel))
+
+@admin.register(CompanyModel)
+class CompanyAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = [field.name for field in CompanyModel._meta.fields]
+
+
+# @admin.register(CompanyModel)
+# class CompanyAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+#     list_display = [field.name for field in CompanyModel._meta.fields]
