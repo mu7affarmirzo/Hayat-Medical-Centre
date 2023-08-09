cac49e91ff46d46fab09324519d57ec01e20a8a4
Johadev05 <javohirsidikxonov@gmail.com>
2023-08-09 17:27:55 +0500

    update

---
+++ backend/apps/hospital/pagination.py
@@ -0,0 +1,35 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.core import paginator
+from django.template.loader import render_to_string
+
+from apps.account.models import PatientModel
+
+NEWS_COUNT_PER_PAGE = 5
+
+
+def pagination(request):
+    global patient
+    page = int(request.GET.get('page', 1))
+    posts = PatientModel.objects.all().order_by('id')
+    p = paginator.Paginator(posts,
+                            NEWS_COUNT_PER_PAGE)
+    try:
+        post_page = p.page(page)
+    except paginator.EmptyPage:
+        post_page = paginator.Page([], page, p)
+
+    if not request.is_ajax():
+        return render(request,
+                      'Hospital/patients-my-patients.html', {'patients': post_page})
+    else:
+        content = ''
+        for patient in post_page:
+            content += render_to_string('Hospital/patients-my-patients.html',
+                                        {'patients': patient},
+                                        request=request)
+        return JsonResponse({
+            'patients': patient,
+            "content": content,
+            "end_pagination": True if page >= p.num_pages else False,
+        })
--- backend/apps/hospital/urls.py
+++ backend/apps/hospital/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import *
+from .pagination import pagination
 
 urlpatterns = [
     path('', dispatching_m_p, name='dispatching_m_p'),
     path('dispatching_p_g/', dispatching_p_g, name='dispatching_p_g'),
     path('dispatching_p_h/', dispatching_p_h, name='dispatching_p_h'),
-    path('dispatching_p_i/',  dispatching_p_i, name='dispatching_p_i'),
-    path('dispatching_p_s/',  dispatching_p_s, name='dispatching_p_s'),
+    path('dispatching_p_i/', dispatching_p_i, name='dispatching_p_i'),
+    path('dispatching_p_s/', dispatching_p_s, name='dispatching_p_s'),
     path('events/', events, name='events'),
     path('login/', login, name='login'),
     path('patients_a/', patients_a, name='patients_a'),
@@ -21,5 +22,6 @@ urlpatterns = [
     path('register_o/', register_o, name='register_o'),
     path('register_p/', register_p, name='register_p'),
     path('register_p_s/', register_p_s, name='register_p_s'),
-    path('register_s/', register_s, name='register_s')
+    path('register_s/', register_s, name='register_s'),
+    path('patients_m_p/pagination/', pagination, name='patients_m_p_paginator'),
 ]
