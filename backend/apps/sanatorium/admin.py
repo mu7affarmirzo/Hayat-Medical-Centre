f3c02d773245033ee27bdfd5125376924d2795a7
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2024-01-16 15:12:10 +0500

    added: admin sanatorium

--- backend/apps/sanatorium/admin.py
+++ backend/apps/sanatorium/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from apps.sanatorium.models import *
+
 # Register your models here.
+admin.site.register(IllnessHistory)
+admin.site.register(AmbulatoryHistory)
+admin.site.register(ChequeModel)
+admin.site.register(DiagnosisTemplate)
+admin.site.register(ProfessionsModel)
+admin.site.register(PivotPillsDrugsModel)
