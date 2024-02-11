575321cdecff347a26c52cb2118c1f83f415b131
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2024-02-11 10:52:37 +0500

    ➕Added: dispatch📞 patients

---
+++ backend/api/v1/sanatorium/views/dispatch.py
@@ -0,0 +1,16 @@
+from django.db.models import Q, Count
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from api.v1.sanatorium.services import dispatch
+
+
+@swagger_auto_schema(tags=['sanatorium'], method="get", )
+@api_view(['GET', ])
+@permission_classes((IsAuthenticated,))
+def get_patients(request):
+    response = dispatch.get_patients(request)
+    return Response(response)
