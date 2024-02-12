b7aaaf1929b9426685d3f171eea62c57d46d9661
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2024-02-12 22:13:53 +0500

    🔄Updated: dispatch📞 remove duplicate

--- backend/api/v1/sanatorium/services/dispatch.py
+++ backend/api/v1/sanatorium/services/dispatch.py
@@ -4,18 +4,18 @@ from api.v1.sanatorium.serializers import *
 from apps.sanatorium.models import *
 
 
-def get_patients(request):
-    user = request.user
-    patients_response = []
-    patients = IllnessHistory.objects.filter(doctor=user)
-    for patient in patients:
-        patients_response.append({
-            "cito": None, "no": patient.series_number, "age": patient.patient.age,
-            "room": patient.booking.room.room_number, "dispatch": patient.diagnosis.name,
-            "start_date": patient.booking.start_date, "end_date": patient.booking.end_date,
-            "doctor": patient.doctor.full_name, "name": patient.patient.full_name
-        })
-    return patients_response
+# def get_patients(request):
+#     user = request.user
+#     patients_response = []
+#     patients = IllnessHistory.objects.filter(doctor=user)
+#     for patient in patients:
+#         patients_response.append({
+#             "cito": None, "no": patient.series_number, "age": patient.patient.age,
+#             "room": patient.booking.room.room_number, "dispatch": patient.diagnosis.name,
+#             "start_date": patient.booking.start_date, "end_date": patient.booking.end_date,
+#             "doctor": patient.doctor.full_name, "name": patient.patient.full_name
+#         })
+#     return patients_response
 
 
 def get_patients(request):
