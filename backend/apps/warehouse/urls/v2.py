713cc401a5472cc32a695ce8c666e9969c7d4c34
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2024-06-05 23:36:53 +0500

    items search

--- backend/apps/warehouse/urls/v2.py
+++ backend/apps/warehouse/urls/v2.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.warehouse.views.v2 import auth
+from apps.warehouse.views.v2 import auth, items
 
 app_name = 'warehouse_v2'
 
 urlpatterns = [
     path('login/', auth.login_view, name='v2-login'),
+    path('items/', items.get_items, name='v2-items'),
+    path('items-search/', items.search_items, name='v2-items-search'),
 ]
