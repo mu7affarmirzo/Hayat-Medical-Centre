c3caa21411579fd93c6dec0348f23028f3cc8352
Музаффармирзо Чориев <muzaffarmirzo.choriyev@ucell.uz>
2024-06-23 13:48:13 +0500

    ➕Added: manual

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('warehouse/v1/', include('apps.warehouse.urls.v1')),
-    path('warehouse/v2/', include('apps.warehouse.urls.v2')),
+    path('warehouse/', include('apps.warehouse.urls.v2')),
+    path('warehouse/manual', include('apps.warehouse.urls.manual')),
     # path('hospital/', include('apps.hospital.urls')),
 
 ]
