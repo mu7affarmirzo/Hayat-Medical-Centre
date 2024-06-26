6dfbdc7c73eacb0f74673f3ec2c616770821b0f4
Музаффармирзо Чориев <muzaffarmirzo.choriyev@ucell.uz>
2024-06-26 14:09:09 +0500

    🔼Updated: storepointstaff added

--- backend/apps/warehouse/models/store_point.py
+++ backend/apps/warehouse/models/store_point.py
@@ -33,3 +33,14 @@ class StorePointModel(models.Model):
 #
 #     class Meta:
 #         unique_together = ('staff', 'store_point')
+
+class StorePointStaffModel(models.Model):
+    staff = models.ForeignKey(Account, related_name="store_staff", on_delete=models.SET_NULL, null=True, blank=True)
+    store_point = models.ForeignKey(StorePointModel, related_name="store_point_staff", on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.ForeignKey(StorePointRolesModel, related_name="store_point_role", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.store_point} - {self.role}"
+
+    class Meta:
+        unique_together = ('staff', 'store_point')
