75e094fbb29df83da04aeecf2ce949bb42a0da53
Музаффармирзо Чориев <muzaffarmirzo.choriyev@ucell.uz>
2024-06-26 14:07:02 +0500

    🔼Updated: storepointstaff removed

--- backend/apps/warehouse/urls/branch.py
+++ backend/apps/warehouse/urls/branch.py
@@ -4,6 +4,6 @@ from apps.warehouse.views.branches import income
 app_name = 'warehouse_branch'
 
 urlpatterns = [
-    path('income/', income.branch_income_view, name='income-list'),
+    # path('income/', income.branch_income_view, name='income-list'),
     path('income/detailed/<int:pk>', income.detailed_income_view, name='income-detailed'),
 ]
