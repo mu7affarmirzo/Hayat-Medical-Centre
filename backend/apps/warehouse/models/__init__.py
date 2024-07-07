5adb425962e962520408d41212ea09deec5d3683
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2024-07-07 17:22:05 +0500

    store points crud

--- backend/apps/warehouse/models/__init__.py
+++ backend/apps/warehouse/models/__init__.py
@@ -2,7 +2,7 @@ from .items import ItemsModel, ItemsInStockModel
 from .receive_registry import ReceiveRegistryModel, ReceivedItemsModel
 from .send_registry import SendRegistryModel, SentItemsModel
 from .income import IncomeModel, IncomeItemsModel
-from .company import CompanyModel, NewStorePointStaffModel
+from .company import CompanyModel
 from .store_point import StorePointModel
 from .cheque import WarehouseChequeModel, ChequeItemsModel
 from .expense import *
