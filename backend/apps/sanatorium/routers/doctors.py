27e73969a7ccf7772afa8db0b3e675ff73b86595
Музаффармирзо Чориев <muzaffarmirzo.choriyev@ucell.uz>
2024-09-03 21:26:58 +0500

    🔼Updated: nurse for migrations

--- backend/apps/sanatorium/routers/doctors.py
+++ backend/apps/sanatorium/routers/doctors.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 
-from apps.logus.views import auth
+from apps.sanatorium.views import doctors
 
 app_name = 'sanatorium_doctors'
 
 urlpatterns = [
-    path('', auth.login_view, name='login'),
-    path('logout/', auth.logout_view, name='logout'),
-    path('main_screen/', auth.main_screen_view, name='main_screen'),
+    path('main_screen/', doctors.main_screen_view, name='main_screen'),
+    path('patients/', doctors.get_patients_list, name='patients'),
+    path('get-patient/<int:pk>', doctors.get_patient_by_id_view, name='get_patient_by_id'),
+    path('notifications/', doctors.main_screen_view, name='notifications'),
+    path('get-bookings-by-start-date', doctors.get_bookings_by_start_date_view, name='by-start-date'),
 
 ]
